c4eda0d3eb692745ab21b8da23fc3d68736942a9
Jordan Justen <jordan.l.justen@intel.com>
2014-02-13 01:21:45 -0800

    run-with-src-dri.sh: If $DISPLAY is not set, then use GBM for piglit
    Signed-off-by: Jordan Justen <jordan.l.justen@intel.com>

--- run-with-src-dri.sh
+++ run-with-src-dri.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 source $HOME/src/fdo/menv
 export LIBGL_DRIVERS_PATH=$HOME/src/fdo/mesa/lib
+export GBM_DRIVERS_PATH=$HOME/src/fdo/mesa/lib
+if [ -z $PIGLIT_PLATFORM -a -z $DISPLAY ]; then
+  export PIGLIT_PLATFORM=gbm
+fi
 $*
