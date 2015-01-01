4d467b51eeab39b0abade335a949387c3dd926bb
Jordan Justen <jordan.l.justen@intel.com>
2015-01-01 22:43:59 +0000

    run-with-src-dri.sh: add quotes around variables

--- run-with-src-dri.sh
+++ run-with-src-dri.sh
@@ -2,7 +2,7 @@
 source $HOME/src/fdo/menv
 export LIBGL_DRIVERS_PATH=$HOME/src/fdo/mesa/lib
 export GBM_DRIVERS_PATH=$HOME/src/fdo/mesa/lib
-if [ -z $PIGLIT_PLATFORM -a -z $DISPLAY ]; then
+if [ -z "$PIGLIT_PLATFORM" -a -z "$DISPLAY" ]; then
   export PIGLIT_PLATFORM=gbm
 fi
 $*
