6dc270e62ac2d70b9f7fc068a71800546ec06f9d
Jordan Justen <jordan.l.justen@intel.com>
2015-03-23 16:33:37 -0700

    Add cs.sh to test compute shaders

---
+++ cs.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#export PIGLIT_PLATFORM=gbm
+export MESA_GL_VERSION_OVERRIDE=4.3
+export MESA_GLSL_VERSION_OVERRIDE=430
+export MESA_EXTENSION_OVERRIDE=GL_ARB_compute_shader
+$HOME/src/fdo/run-with-src-dri.sh $*
