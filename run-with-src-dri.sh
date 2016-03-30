5c0d62144dbc9c5faeb2f63b3b3db2a3a5cf2f03
Jordan Justen <jordan.l.justen@intel.com>
2016-03-30 12:05:28 -0700

    run-with-src-dri.sh: Add GDB env var support
    Signed-off-by: Jordan Justen <jordan.l.justen@intel.com>

--- run-with-src-dri.sh
+++ run-with-src-dri.sh
@@ -2,7 +2,13 @@
 source $HOME/src/fdo/menv
 export LIBGL_DRIVERS_PATH=$HOME/src/fdo/mesa/lib
 export GBM_DRIVERS_PATH=$HOME/src/fdo/mesa/lib
+: ${GDB=0}
+if [ "$GDB" == "0" ]; then
+  GDB=""
+else
+  GDB="gdb --args"
+fi
 if [ -z "$PIGLIT_PLATFORM" -a -z "$DISPLAY" ]; then
   export PIGLIT_PLATFORM=gbm
 fi
-$*
+$GDB $*
