465f91ee52a264664df7fbeb9fbbcebd7f8853cf
Jordan Justen <jordan.l.justen@intel.com>
2017-03-23 16:56:42 -0700

    run-with-src-dri.sh: Fix running executable with space
    Signed-off-by: Jordan Justen <jordan.l.justen@intel.com>

--- run-with-src-dri.sh
+++ run-with-src-dri.sh
@@ -5,18 +5,17 @@ export LIBGL_DRIVERS_PATH=$MESA_DIR/lib
 export GBM_DRIVERS_PATH=$MESA_DIR/lib
 
 : ${GDB=0}
-if [ "$GDB" == "0" ]; then
-  GDB=""
-else
-  GDB="gdb --args"
+if [ "$GDB" != "0" ]; then
+    set - gdb --args "$@"
 fi
+
 : ${AUB=0}
-if [ "$AUB" == "0" ]; then
-  AUB=""
-else
-  AUB="intel_aubdump"
+if [ "$AUB" != "0" ]; then
+    set - intel_aubdump "$@"
 fi
+
 if [ -z "$PIGLIT_PLATFORM" -a -z "$DISPLAY" ]; then
   export PIGLIT_PLATFORM=gbm
 fi
-$AUB $GDB $*
+
+exec "$@"
